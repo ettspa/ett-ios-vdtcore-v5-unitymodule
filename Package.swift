743f374eb2de2e615381cd8185e85272f70b5d98
Marco Mirisola <marco.mirisola@grupposcai.it>
2024-12-13 10:26:47 +0100

    0.0.3

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
             dependencies: ["UnityFramework"]
         ),
         .binaryTarget(name: "UnityFramework",
-                      url: "https://github.com/MarcoMirisola/Test/releases/download/0.0.2/UnityFramework.xcframework.zip",
-                      checksum: "8fecfb563dc7f5f5022680ecbc031567846f0806d43c6034a9f2887d7a4d24d6"),
+                      url: "https://github.com/MarcoMirisola/Test/releases/download/0.0.3/UnityFramework.xcframework.zip",
+                      checksum: "3e323849dc3f188eda359fb6df1a6c2edf11b057ad8577f195aeaba660d34e55"),
 
     ]
 )
