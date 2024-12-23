4d35abc7f6515b0b5eef256f5ea9816c4eabdae8
Marco Mirisola <marco.mirisola@grupposcai.it>
2024-12-23 16:36:10 +0100

    0.0.9

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
             dependencies: ["UnityFramework"]
         ),
         .binaryTarget(name: "UnityFramework",
-                      url: "https://github.com/ettspa/ett-ios-vdtcore-v5-unitymodule/releases/download/0.0.8/UnityFramework.xcframework.zip",
-                      checksum: "f4d4c5693a3ec9ed26415a2edd67fcba364a2c3e3d3d1595d5d4372a9202fcbe"),
+                      url: "https://github.com/ettspa/ett-ios-vdtcore-v5-unitymodule/releases/download/0.0.9/UnityFramework.xcframework.zip",
+                      checksum: "64dd4ab1a1c18248301ce3f9dbdc01a0656615eb5bdfc19b5a75049b208f5934"),
 
     ]
 )
